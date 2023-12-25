beaa3b30132237b618501629aec0197e688582d1
Chiraagkv <69423507+Chiraagkv@users.noreply.github.com>
2023-12-25 20:02:36 +0530

    first commit
    added project files

---
+++ trials.py
@@ -0,0 +1,125 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+from mediapipe.python.solutions import hands_connections
+from mediapipe.python.solutions.drawing_utils import DrawingSpec
+from mediapipe.python.solutions.hands import HandLandmark
+from typing import Mapping, Tuple
+_RADIUS = 2
+_RED = (48, 48, 255)
+_GREEN = (48, 255, 48)
+_BLUE = (192, 101, 21)
+_YELLOW = (0, 204, 255)
+_GRAY = (128, 128, 128)
+_PURPLE = (128, 64, 128)
+_PEACH = (180, 229, 255)
+_WHITE = (224, 224, 224)
+_CYAN = (192, 255, 48)
+_MAGENTA = (192, 48, 255)
+
+# Hands
+_THICKNESS_WRIST_MCP = 2
+_THICKNESS_FINGER = 1
+_THICKNESS_DOT = -1
+_PALM_LANDMARKS = (HandLandmark.WRIST, HandLandmark.THUMB_CMC,
+                   HandLandmark.INDEX_FINGER_MCP,
+                   HandLandmark.MIDDLE_FINGER_MCP, HandLandmark.RING_FINGER_MCP,
+                   HandLandmark.PINKY_MCP)
+_THUMP_LANDMARKS = (HandLandmark.THUMB_MCP, HandLandmark.THUMB_IP,
+                    HandLandmark.THUMB_TIP)
+_INDEX_FINGER_LANDMARKS = (HandLandmark.INDEX_FINGER_PIP,
+                           HandLandmark.INDEX_FINGER_DIP,
+                           HandLandmark.INDEX_FINGER_TIP)
+_MIDDLE_FINGER_LANDMARKS = (HandLandmark.MIDDLE_FINGER_PIP,
+                            HandLandmark.MIDDLE_FINGER_DIP,
+                            HandLandmark.MIDDLE_FINGER_TIP)
+_RING_FINGER_LANDMARKS = (HandLandmark.RING_FINGER_PIP,
+                          HandLandmark.RING_FINGER_DIP,
+                          HandLandmark.RING_FINGER_TIP)
+_PINKY_FINGER_LANDMARKS = (HandLandmark.PINKY_PIP, HandLandmark.PINKY_DIP,
+                           HandLandmark.PINKY_TIP)
+_HAND_LANDMARK_STYLE = {
+    _PALM_LANDMARKS:
+        DrawingSpec(
+            color=_RED, thickness=_THICKNESS_DOT, circle_radius=_RADIUS),
+    _THUMP_LANDMARKS:
+        DrawingSpec(
+            color=_PEACH, thickness=_THICKNESS_DOT, circle_radius=_RADIUS),
+    _INDEX_FINGER_LANDMARKS:
+        DrawingSpec(
+            color=_PURPLE, thickness=_THICKNESS_DOT, circle_radius=_RADIUS),
+    _MIDDLE_FINGER_LANDMARKS:
+        DrawingSpec(
+            color=_YELLOW, thickness=_THICKNESS_DOT, circle_radius=_RADIUS),
+    _RING_FINGER_LANDMARKS:
+        DrawingSpec(
+            color=_GREEN, thickness=_THICKNESS_DOT, circle_radius=_RADIUS),
+    _PINKY_FINGER_LANDMARKS:
+        DrawingSpec(
+            color=_BLUE, thickness=_THICKNESS_DOT, circle_radius=_RADIUS),
+}
+_HAND_CONNECTION_STYLE = {
+    hands_connections.HAND_PALM_CONNECTIONS:
+        DrawingSpec(color=_GRAY, thickness=_THICKNESS_WRIST_MCP),
+    hands_connections.HAND_THUMB_CONNECTIONS:
+        DrawingSpec(color=_PEACH, thickness=_THICKNESS_FINGER),
+    hands_connections.HAND_INDEX_FINGER_CONNECTIONS:
+        DrawingSpec(color=_PURPLE, thickness=_THICKNESS_FINGER),
+    hands_connections.HAND_MIDDLE_FINGER_CONNECTIONS:
+        DrawingSpec(color=_YELLOW, thickness=_THICKNESS_FINGER),
+    hands_connections.HAND_RING_FINGER_CONNECTIONS:
+        DrawingSpec(color=_GREEN, thickness=_THICKNESS_FINGER),
+    hands_connections.HAND_PINKY_FINGER_CONNECTIONS:
+        DrawingSpec(color=_BLUE, thickness=_THICKNESS_FINGER)
+}
+def get_default_hand_landmarks_style() -> Mapping[int, DrawingSpec]:
+  hand_landmark_style = {}
+  for k, v in _HAND_LANDMARK_STYLE.items():
+    for landmark in k:
+      hand_landmark_style[landmark] = v
+  return hand_landmark_style
+
+def get_default_hand_connections_style(
+) -> Mapping[Tuple[int, int], DrawingSpec]:
+  hand_connection_style = {}
+  for k, v in _HAND_CONNECTION_STYLE.items():
+    for connection in k:
+      hand_connection_style[connection] = v
+  return hand_connection_style
+
+base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+def draw_landmarks_on_image(frame, truth=False):
+  image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+  detection_result = detector.detect(image)
+  hand_landmarks_list = detection_result.hand_landmarks
+  annotated_image = np.zeros(frame.shape)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+
+    # Draw the hand landmarks.
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks_proto,
+      solutions.hands.HAND_CONNECTIONS,
+      get_default_hand_landmarks_style(),
+      get_default_hand_connections_style())
+
+  return annotated_image
+img = cv2.imread("hands1.jpg")
+out = draw_landmarks_on_image(img)
+cv2.imshow("out", out)
+cv2.waitKey(0) 
+cv2.destroyAllWindows() 
